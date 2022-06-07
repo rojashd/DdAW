b443ea88b6ea606d076d285dd30b6e6889b0538c
Hector Rojas <104121245+rojashd@users.noreply.github.com>
2022-06-06 21:37:32 -0600

    ejercicio 1
    syntax

---
+++ LabS7/js/main.js
@@ -0,0 +1,11 @@
+var user = ["fname" , "password"];
+
+var fname = prompt ("Ingresa tu nombre de usuario");
+var password = prompt ("Ingresa tu contraseña");
+user.push();
+
+if (user [0] == "Messi" && user [1] == "Goat"){
+    document.write("¡¡“Bienvenido Messi!!”.");
+}else {
+    document.write("Usuario no valido.");
+}
