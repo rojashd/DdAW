3eae07b0bbb81430f5c47b9cfcf22bb8e7e4c624
Hector Rojas <104121245+rojashd@users.noreply.github.com>
2022-06-28 12:09:42 -0600

    last commit
    se paso la clase

---
+++ Examen Primer Parcial/js/main.js
@@ -0,0 +1,52 @@
+let iniciales = document.getElementById("iniciales");
+
+let inicialesDatos = [{
+    id:"squirtle"
+    precio: 20,
+    img: "img/squirtle.png"
+},
+{
+    id:"charmander"
+    precio: 25,
+    img: "img/charmander.png"
+},
+{
+    id:"bulbasaur"
+    precio: 30,
+    img: "img/bulbasaur.png"
+},
+{
+    id:"totodile"
+    precio: 20,
+    img: "img/totodile.png"
+},
+{
+    id:"cyndaquil"
+    precio: 25,
+    img: "img/cyndaquil.png"
+},
+{
+    id:"chikorita"
+    precio: 30,
+    img: "img/chikorita.png"
+},
+{
+    id:"mudkip"
+    precio: 20,
+    img: "img/mudkip.png"
+},
+{
+    id:"torchic"
+    precio: 25,
+    img: "img/torchic.png"
+},
+{
+    id:"treecko"
+    precio: 30,
+    img: "img/treecko.png"
+}]
+
+
+let incrementar  = () => {};
+let reducir = () => {};
+let actualizar = () => {};
